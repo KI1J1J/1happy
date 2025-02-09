aad0491d4c5b683845f434d46591896c00bfe18e
KI1J1J <wlswkfla820@gmail.com>
2025-02-09 18:55:55 +0900

    Create layout.js

---
+++ app/app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: '티켓팅 정보 입력',
+  description: '티켓팅 정보를 입력하세요',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
